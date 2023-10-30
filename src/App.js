4fb4c41ecd4b6702d4f1d89f7ba00e6a92e999a2
BoshraM <gbboshra@gmail.com>
2023-10-30 19:59:20 +0000

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Hero } from "./components/Hero/Hero";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+
 
 function App() {
   return (
+
     <div className="App">
+
+
+      {/* <Navbar /> */}
+      {/* <Resume /> */}
+      <Hero />
+      <Projects />
+      <Contact />
+
     </div>
+
   );
 }
 
