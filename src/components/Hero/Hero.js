493719335bfe8fa2ebaf281c00b8e8fca5ff190f
BoshraM <gbboshra@gmail.com>
2023-10-30 19:58:06 +0000

    update hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,5 @@
-import { Link } from "react-scroll"
 import "./hero.css"
-import picture from "../../assests/IMG_0607.jpg"
+import picture from "../../assests/PHOTO-2023-10-28-14-44-23.jpg"
 import { AiFillLinkedin } from "react-icons/ai";
 import { AiFillGithub } from "react-icons/ai";
 import { AiOutlineMail } from "react-icons/ai";
@@ -16,23 +15,32 @@ export const Hero = () => {
                     <p className="profile_name">Boshra Mahmoudi</p>
 
                     <p className="profile_role">Software Developer</p>
-                    <Link><button className="profile_btn">Hire Me</button></Link>
+                    <button className="profile_btn">Hire Me</button>
                 </div>
                 <div className="hero_socials flex">
-                    <AiFillLinkedin className='icon' />
-                    <AiFillGithub className="icon" />
-                    <AiOutlineMail className='icon' />
+                    <a href="https://www.linkedin.com/in/boshra-mahmoudi/" target="blank" aria-label="icons to navigate to linkedin">
+                        <AiFillLinkedin className='icon' />
+                    </a>
+                    <a href="https://github.com/BoshraM" target="blank" aria-label="icons to navigate to github">
+                        <AiFillGithub className="icon" />
+                    </a>
+                    <a href="mailto:gbBoshra@gmail.com" target="_blank" aria-label="icons to navigate to email" rel="noreferrer">
+                        <AiOutlineMail className='icon' />
+                    </a>
+
+
+
                 </div>
 
             </div>
             <div className="about_container">
                 <div className="about_content">
                     <h1>About Me</h1>
-                    <p>a full-stack developer graduate of Code Your Future, I am passionate about using technology to solve problems and face challenges that allow me to put my problem-solving skills into action. Participating in different projects at Code Your Future has allowed me to develop great solutions and collaborate with other developers while building the communication and technical skills needed to excel in any work environment. This career has allowed me to learn new skills and boost my confidence.</p>
+                    <p>I'm a Full-stack developer based in the UK, where I live with my family and energetic son. With a lifelong love for games, puzzles, and books, along with a deep passion for nature, I'm ready to make a tech-powered impact in the world of software development.</p>
                 </div>
                 <div className="about_link">
-                    <Link><button className="about_btn">Resume</button></Link>
-                    <Link><button className="about_btn">Projects</button></Link>
+                    <button className="about_btn">Resume</button>
+                    <button className="about_btn">Projects</button>
 
                 </div>
 
