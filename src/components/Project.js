23b49b85f7aa199191dd24ecca99a5483c5cc7f2
BoshraM <gbboshra@gmail.com>
2023-10-30 19:54:19 +0000

    improve accessibilty of projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,13 +4,14 @@ import { AiFillGithub } from "react-icons/ai";
 // Define an array of project data
 
 
-export const Project = ({ img, title, liveLink, githubLink }) => (
+export const Project = ({ img, title, liveLink, githubLink, date }) => (
     <div className="project_container">
         <img src={img} alt={title} className="project_img" />
-        <a href={liveLink} target="_blank" className="project_link" rel="noreferrer">
+        <a href={liveLink} target="_blank" className="project_link" rel="noreferrer" aria-label="icons to navigate to project demo">
             {title}
         </a>
-        <a href={githubLink} target="_blank" rel="noreferrer">
+        <p>{date}</p>
+        <a href={githubLink} target="_blank" rel="noreferrer" aria-label="icons to navigate to github repository">
             <AiFillGithub className="icon" />
         </a>
         <details></details>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,24 +9,42 @@ const projectsData = [
         title: "CYF PROGRESS Tracker",
         liveLink: "https://starter-kit-j8jj.onrender.com/",
         githubLink: "https://github.com/BoshraM/ldn9-Ctrl-Shift-Learn",
+        date: "JUL 2023"
     },
     {
         img: require("../assests/full-stack.png"),
         title: "Video Recommendation",
         liveLink: "https://fullstack-assesment-egxj.onrender.com/",
         githubLink: "https://github.com/BoshraM/Full-Stack-Project-Assessment",
+        date: "JUN 2023"
     },
     {
         img: require("../assests/node-farm.png"),
         title: "Node Farm",
         liveLink: "https://node-farm-boshram.glitch.me/",
         githubLink: "https://github.com/BoshraM/my-complete-node-bootcamp",
+        date: "MAY 2023"
     },
     {
-        img: require("../assests/node-farm.png"),
-        title: "Node Farm",
-        liveLink: "https://node-farm-boshram.glitch.me/",
-        githubLink: "https://github.com/BoshraM/my-complete-node-bootcamp",
+        img: require("../assests/buddy project.png"),
+        title: "EMPOWERED",
+        liveLink: "https://empowered.netlify.app/07-combined-preview/01-home/",
+        githubLink: "https://github.com/BoshraM/buddy-group-project",
+        date: "DEC 2023"
+    },
+    {
+        img: require("../assests/amazonshop.png"),
+        title: "Amazon-Clone",
+        liveLink: "https://london9-amazon-clone-50-react-project.netlify.app/",
+        githubLink: "https://github.com/ShayanMahnam/team-amazon-clone",
+        date: "APR 2023"
+    },
+    {
+        img: require("../assests/hotel-react.png"),
+        title: "CYF-Hotel-React",
+        liveLink: "https://cyf-boshram-hotel-react.netlify.app/https://cyf-boshram-hotel-react.netlify.app/",
+        githubLink: "https://github.com/BoshraM/cyf-hotel-react",
+        date: "APR 2023"
     },
 ];
 export const Projects = () => {
